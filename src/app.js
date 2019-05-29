4da5d9630e4f0d466007c9a87c3832ba639e03e4
Richard Yarwood <richyarwood@gmail.com>
2019-05-29 11:41:50 +0100

    Register and login errors fixed

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ import { HashRouter as Router, Switch, Route } from 'react-router-dom'
 
 import Home from './components/common/Home'
 import NavBar from './components/common/NavBar'
+import Login from './components/auth/Login'
+import Register from './components/auth/Register'
 
 import 'bulma'
 import './style.scss'
@@ -18,6 +20,8 @@ class App extends React.Component{
         <main>
           <NavBar />
           <Switch>
+            <Route path="/register" component={Register} />
+            <Route path="/login" component={Login} />
             <Route path="/" component={Home} />
           </Switch>
         </main>
---
+++ src/components/auth/Register.js
@@ -0,0 +1,101 @@
+import React from 'react'
+import axios from 'axios'
+
+class Register extends React.Component {
+
+  constructor() {
+    super()
+
+    this.state = {
+      data: {},
+      errors: {}
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(e) {
+    const data = { ...this.state.data, [e.target.name]: e.target.value }
+    this.setState({ data: data })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+
+    axios.post('/api/register', this.state.data)
+      .then(() => this.props.history.push('/login'))
+      .catch(err => this.setState({ errors: err.response.data.error }))
+  }
+
+  render() {
+    console.log(this.state.data)
+    console.log(this.state.errors)
+    return (
+      <section className="section">
+        <div className="container">
+          <div className="columns is-centered">
+            <div className="column is-half-desktop is-two-thirds-tablet">
+              <div className="title is-3">Register</div>
+              <form onSubmit={this.handleSubmit}>
+                <div className="field">
+                  <label className="label">Username</label>
+                  <div className="control">
+                    <input
+                      className="input"
+                      name="username"
+                      placeholder="eg: richie"
+                      onChange={this.handleChange}
+                    />
+                  </div>
+                  {this.state.errors.username && <div className="help is-danger">{this.state.errors.username}</div>}
+                </div>
+                <div className="field">
+                  <label className="label">Email</label>
+                  <div className="control">
+                    <input
+                      className="input"
+                      name="email"
+                      placeholder="eg: richie@email.com"
+                      onChange={this.handleChange}
+                    />
+                  </div>
+                  {this.state.errors.email && <div className="help is-danger">{this.state.errors.email}</div>}
+                </div>
+                <div className="field">
+                  <label className="label">Password</label>
+                  <div className="control">
+                    <input
+                      className="input"
+                      name="password"
+                      type="password"
+                      placeholder="eg: ••••••••"
+                      onChange={this.handleChange}
+                    />
+                  </div>
+                  {this.state.errors.password && <div className="help is-danger">{this.state.errors.password}</div>}
+                </div>
+                <div className="field">
+                  <label className="label">Password Confirmation</label>
+                  <div className="control">
+                    <input
+                      className="input"
+                      name="password_confirmation"
+                      type="password"
+                      placeholder="eg: ••••••••"
+                      onChange={this.handleChange}
+                    />
+                  </div>
+                  {this.state.errors.password_confirmation && <div className="help is-danger">{this.state.errors.password_confirmation}</div>}
+                </div>
+                <button className="button is-primary">Submit</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    )
+  }
+}
+
+export default Register
