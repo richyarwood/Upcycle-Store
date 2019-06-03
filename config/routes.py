677bea6580fc4c78980e70e641de8c17d3aa86ee
Richard Yarwood <richyarwood@gmail.com>
2019-06-03 14:58:35 +0100

    Updated deployment scripts

--- config/routes.py
+++ config/routes.py
@@ -1,3 +1,4 @@
+import os
 from app import app
 from controllers import listings, categories, auth, cart_items
 
@@ -5,3 +6,12 @@ app.register_blueprint(listings.router, url_prefix='/api')
 app.register_blueprint(categories.router, url_prefix='/api')
 app.register_blueprint(auth.router, url_prefix='/api')
 app.register_blueprint(cart_items.router, url_prefix='/api')
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+
+    if os.path.isfile('public/' + path):
+        return app.send_static_file(path)
+
+    return app.send_static_file('index.html')
